7a49f4a869cbcedc8ac8d2826412257b469a1aa9
Enntek <abluecar@gmail.com>
2022-07-23 18:58:21 -0400

    Add comments, get pseudo code going first

--- tictactoe.rb
+++ tictactoe.rb
@@ -1,3 +1,29 @@
 # Odin Ruby Project: Tic Tac Toe
 # https://www.theodinproject.com/lessons/ruby-tic-tac-toe
 
+
+# state the problem, work on possible solutions
+# begin game with 2 players, establish o and x for player 1 and player 2
+# each player takes a turn until game ends
+# players input like this: R1C1 (row 1 column 1)
+# check for victory in a loop
+
+# what is victory?
+# 3 x's or 3 o's in a row
+
+# player 1 is circle, select a tile:
+# R1C1
+
+# display a board in the console like so??? doesn't look right
+#   O |  | 
+#   --------
+#     |  |
+#   --------
+#     |  |
+
+# scan for winners...
+# logic: if r1c1 is circle, check r1c2, if r1c2 is a circle, check r1c3, if r1c3 is a circle, winner!
+# but... no winner yet
+
+# player 2 is circle, select a tile:
+# r3c2
