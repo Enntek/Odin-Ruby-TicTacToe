358c30b597f3e6e1de2b940b32400d4de3189972
sam3 <cwm9889@gmail.com>
2022-07-24 16:15:34 -0400

    Work more logic starting with player 1

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# Odin-Ruby-TicTacToe
+# Odin Ruby Project: Tic Tac Toe
+# https://www.theodinproject.com/lessons/ruby-tic-tac-toe
+
+This project is the first exercise to apply our knowledge of Ruby classes,
+modules, inheritance that we recently had to study.
+
+Description from Odin: Build a tic-tac-toe game on the command line where two human players can play against each other and the board is displayed in between turns.
+
--- tictactoe.rb
+++ tictactoe.rb
@@ -2,6 +2,19 @@
 # https://www.theodinproject.com/lessons/ruby-tic-tac-toe
 
 
+# how do we use classes to implement this?
+# create game without classes, see if the need for classes will arise naturally
+# if you finish the game without using a single class, then seek other solutions.
+
+puts "You are circle, select a tile 1 - 9: "
+player1_action = gets.chomp
+
+puts player1_action
+
+row1 = ["e", "e", "e"]
+row2 = ["e", "e", "e"]
+row3 = ["e", "e", "e"]
+
 # state the problem, work on possible solutions
 # begin game with 2 players, establish o and x for player 1 and player 2
 # each player takes a turn until game ends
@@ -15,11 +28,9 @@
 # R1C1
 
 # display a board in the console like so??? doesn't look right
-#   O |  | 
-#   --------
-#     |  |
-#   --------
-#     |  |
+#   OXO
+#   X__
+#   __O
 
 # scan for winners...
 # logic: if r1c1 is circle, check r1c2, if r1c2 is a circle, check r1c3, if r1c3 is a circle, winner!
@@ -27,3 +38,6 @@
 
 # player 2 is circle, select a tile:
 # r3c2
+
+# etc.. 
+# but how do classes fit into this???
