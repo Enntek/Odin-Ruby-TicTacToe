6e21a86283b61ec08ac27360f1ed809a6e5b331f
Enntek <abluecar@gmail.com>
2022-07-25 00:32:04 -0400

    Disallow entering an occupied space

--- tictactoe.rb
+++ tictactoe.rb
@@ -1,6 +1,9 @@
 # Odin Ruby Project: Tic Tac Toe
 # https://www.theodinproject.com/lessons/ruby-tic-tac-toe
 
+# game logic is complete, clean up and refactor code 
+# add a play again? option
+
 class Player
   attr_reader :name
   attr_accessor :symbol
@@ -18,13 +21,32 @@ class Player
   end
 
   def play_turn
-    puts "#{@name} it's your turn, you are #{@symbol}. select a tile 1 - 9: "
-    begin
-      space = Kernel.gets.chomp.match(/[1-9]/)[0].to_i
-    rescue NoMethodError=>e
-      puts "Invalid input, please press a number 1 - 9."
-      retry
+
+    puts "#{@name} it's your turn, you are #{@symbol}. Select a tile 1 - 9: "
+    
+
+    while true
+      begin
+        Player.display_grid    
+        space = Kernel.gets.chomp.match(/[1-9]/)[0].to_i
+      rescue NoMethodError=>e
+        puts "Invalid input, please press a number 1 - 9."
+        retry
+      end
+
+      flat_grid = @@game_board.flatten
+
+      # p @@game_board
+      # p flat_grid
+      # p flat_grid[space + 1]
+
+      break if flat_grid[space - 1] == " " #check if space is untaken
+      puts "That space is already taken! Enter a new number 1 - 9"
     end
+
+    puts "outside of while loop"
+
+
     @spaces.push(space)
     
     if space < 4
@@ -41,9 +63,11 @@ class Player
   end
 
   def self.display_grid
+    puts "\n"
     puts "\t#{@@game_board[0]}"
     puts "\t#{@@game_board[1]}"
     puts "\t#{@@game_board[2]}"
+    puts "\n"
   end
 
   def self.win_check(name, symbol)
@@ -68,9 +92,9 @@ class Player
     
     #check diagonals
     diagonal_arr = [@@game_board[0][0], @@game_board[1][1], @@game_board[2][2]]
-    diagonal_arr2 = [@@game_board[0][2], @@game_board[1][1], @@game_board[2][0]]
+    diagonal2_arr = [@@game_board[0][2], @@game_board[1][1], @@game_board[2][0]]
     diagonal_win = diagonal_arr.all? { |spot| spot == symbol }
-    diagonal2_win = diagonal_arr2.all? { |spot| spot == symbol }
+    diagonal2_win = diagonal2_arr.all? { |spot| spot == symbol }
     if diagonal_win || diagonal2_win
       win = true
     end
@@ -80,7 +104,7 @@ class Player
   end
 
   def self.end_game(name)
-    puts "<< congrats #{name}!! >>"
+    puts "<< Congrats #{name}! You won! >>"
     @@game_over = true
   end
 end
@@ -90,7 +114,6 @@ player2 = Player.new("Player2")
 
 puts "Player1, choose X or O: "
 
-# player1.symbol = gets.chomp.upcase
 begin
   puts "Please select X or O: "
   player1.symbol = Kernel.gets.chomp.upcase.match(/(X|O)/)[0]
@@ -106,6 +129,7 @@ puts "Player2's symbol is #{player2.symbol}"
 
 while true
   player1.play_turn
+  break if Player.game_over == true
   player2.play_turn
   break if Player.game_over == true
 end
