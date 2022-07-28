97a6c1d21413f887c10b55ad425a142bd82ab454
sam-laptop <cwm9889@gmail.com>
2022-07-28 15:14:15 -0400

    Convert class methods to inst methods

--- tictactoe.rb
+++ tictactoe.rb
@@ -10,6 +10,8 @@
 # blank numbers on game board print
 # eliminate class variables and class methods for now, no reason to use them yet.
 
+# Change all class methods to instance methods
+
 class Game
   @@some_variable = true
   attr_reader :game_board
@@ -18,19 +20,19 @@ class Game
   def initialize
     puts "Welcome to Tic Tac Toe!\n "
     # @@game_board = (0..8).to_a
-    @@game_board = Array.new(9, ' ')
+    @game_board = Array.new(9, ' ')
     @game_over = false
     player1 = Player.new('Player 1')
     player2 = Player.new('Player 2')
-    Game.draw_board
+    draw_board
     game(player1, player2)
   end
 
-  def self.current_board
-    @@game_board
+  def current_board
+    @game_board
   end
 
-  def self.draw_board
+  def draw_board
     puts "\n"
     (0..8).step(3).each do |i|
       puts "\t#{@@game_board[i]} | #{@@game_board[i+1]} | #{@@game_board[i+2]}"
@@ -52,13 +54,13 @@ class Game
     end
   end
 
-  def self.valid_move(move, player_symb, player)
-    @@game_board[move] = player_symb
-    Game.draw_board
-    Game.win_check(player_symb, player)
+  def valid_move(move, player_symb, player)
+    @game_board[move] = player_symb
+    draw_board
+    win_check(player_symb, player)
   end
 
-  def self.win_check(player_symb, player)
+  def win_check(player_symb, player)
     win_configs = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7],
                    [2, 5, 8], [0, 4, 8], [2, 4, 6]]
 
@@ -81,7 +83,7 @@ class Game
 end
 
 class Player
-  @@sides = ['X', 'O']
+  @sides = ['X', 'O']
   attr_reader :name, :side
 
   def initialize(name)
@@ -91,7 +93,7 @@ class Player
   end
 
   def choose_side
-    if @@sides.length == 2
+    if @sides.length == 2
       begin
         puts 'Choose X or O:'
         # @side = "X"
@@ -102,9 +104,9 @@ class Player
       end
 
       puts "#{@name} is #{@side}"
-      @@sides.reject! { |item| item == @side }
+      @sides.reject! { |item| item == @side }
     else
-      @side = @@sides[0]
+      @side = @sides[0]
       puts "#{@name} is #{@side}"
     end
   end
@@ -130,7 +132,7 @@ class Player
       end
     end
 
-    Game.valid_move(valid_spot, @side, @name)
+    valid_move(valid_spot, @side, @name)
   end
 end
 
